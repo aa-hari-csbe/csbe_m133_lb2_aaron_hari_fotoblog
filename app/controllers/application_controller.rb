b698c488943311cb922138bd403c2c86e385739f
csbe-aa-hari <aaron.hari@csbe.ch>
2020-09-24 12:48:07 +0200

    user session added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  included CurrentUserConcern
+  skip_before_action :verify_authenticity_token
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:crate]
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
   root to: "fotoblog#index"
+
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
 end
